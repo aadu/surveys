43a17bcc9c559b7ec3a33bb01dc0bb0871019ffe
Aaron Duke <aaron@0ptimus.com>
2016-08-15 18:48:37 -0700

    Working v1.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // pull in desired CSS/SASS files
 require( './styles/main.scss' );
+var jquery = require('jquery');
+var materialize = require('materialize-css');
 
 // inject bundled Elm app into div#main
 var Elm = require( './Main' );
